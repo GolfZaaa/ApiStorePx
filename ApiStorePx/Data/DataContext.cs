831c82088fe36add7158f1881e9adb414159bb88
GolfZaaa <golf_1234_ag@hotmail.com>
2026-09-05 13:01:35 +0700

    Add DataContext

---
+++ ApiStorePx/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiStorePx.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStorePx.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Restock> Restocks { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleItem> SaleItems { get; set; }
+
+    }
+}
