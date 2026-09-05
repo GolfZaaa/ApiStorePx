26143db493184b3de616a6bd627ed838b4006030
GolfZaaa <golf_1234_ag@hotmail.com>
2026-09-05 16:20:21 +0700

    Add Dtos

---
+++ ApiStorePx/DTOs/Product/CreateProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiStorePx.DTOs.Product
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int SellPrice { get; set; }
+    }
+}
---
+++ ApiStorePx/DTOs/Product/UpdateProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiStorePx.DTOs.Product
+{
+    public class UpdateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int SellPrice { get; set; }
+    }
+}
