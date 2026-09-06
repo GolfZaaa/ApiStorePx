53d893bcac7384faf9852e176e9372f953718b48
GolfZaaa <golf_1234_ag@hotmail.com>
2026-09-06 14:36:46 +0700

    ProductService

---
+++ ApiStorePx/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ApiStorePx.DTOs.Product;
+using ApiStorePx.Models;
+
+namespace ApiStorePx.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> CreateAsync(CreateProductDto dto);
+        Task<Product?> UpdateAsync(int id,UpdateProductDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ ApiStorePx/Services/ProductService.cs
@@ -0,0 +1,76 @@
+﻿using ApiStorePx.Data;
+using ApiStorePx.DTOs.Product;
+using ApiStorePx.Models;
+using ApiStorePx.Services.IServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStorePx.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly DataContext _context;
+
+        public ProductService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _context.Products.OrderBy(x => x.Name).ToListAsync();
+        }
+
+        //sucessfully
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+
+        public async Task<Product> CreateAsync(CreateProductDto dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                SellPrice = dto.SellPrice,
+                Stock = 0,
+                AvgCost = 0
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product?> UpdateAsync(int id, UpdateProductDto dto)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return null;
+            }
+            product.Name = dto.Name;
+            product.SellPrice = dto.SellPrice;
+            product.UpdateAt = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return false;
+            }
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+    }
+}
