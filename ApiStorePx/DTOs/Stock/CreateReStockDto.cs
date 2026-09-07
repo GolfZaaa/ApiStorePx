5350adaae77789ff1d8635e3550af37db0c65fa3
GolfZaaa <golf_1234_ag@hotmail.com>
2026-09-07 12:17:12 +0700

    Add CreateReStockDto

---
+++ ApiStorePx/DTOs/Stock/CreateReStockDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiStorePx.DTOs.Stock
+{
+    public class CreateReStockDto
+    {
+        public int ProductId { get; set; } 
+        public int PackCost { get; set; }
+        public int UnitsPerPack { get; set; }
+        public int Pack { get; set; }
+    }
+}
